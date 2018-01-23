22dc23eae1d9b145e8759a6706efe31f9acf6d34
SilvaEmerson <emersonjose877@gmail.com>
2018-01-22 23:42:54 -0300

    Edit API.py

--- face_recognition/API.py
+++ face_recognition/API.py
@@ -58,5 +58,5 @@ def detect_person(file_name):
     return result[True].rsplit('.', 1)[0]
 
 if __name__ == "__main__":
-    port = int(os.enviroment.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
