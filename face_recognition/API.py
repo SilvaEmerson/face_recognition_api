16bc647945adec6a3520c5789362d53aeebe57e4
SilvaEmerson <emersonjose877@gmail.com>
2018-07-09 10:49:43 -0300

    After run linter

--- face_recognition/API.py
+++ face_recognition/API.py
@@ -1,4 +1,4 @@
-#-*- encoding: utf-8 -*-
+# -*- encoding: utf-8 -*-
 
 import face_recognition as fr
 from flask import Flask, jsonify, request, redirect, render_template
@@ -8,16 +8,16 @@ import json
 import numpy as np
 import requests
 
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'txt', 'npy'}
-UPLOAD_FOLDER = './root/face_recognition/static/train'
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif", "txt", "npy"}
+UPLOAD_FOLDER = "./root/face_recognition/static/train"
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-#manage file
+# manage file
 def allowed_file(filename):
-    return '.' in filename and \
-        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 def detect_person(file_name):
     img = fr.load_image_file(file_name)
@@ -25,15 +25,19 @@ def detect_person(file_name):
 
     # crie um diretório no root com o nome train contendo
     # as imagens de treino
-    know_images = os.listdir('./root/face_recognition/static/train')
+    know_images = os.listdir("./root/face_recognition/static/train")
 
-    know_images_encoded = [np.load('./root/face_recognition/static/train/' + i) for i in know_images]
+    know_images_encoded = [
+        np.load("./root/face_recognition/static/train/" + i) for i in know_images
+    ]
     # know_images_encoded = [fr.face_encodings(fr.load_image_file('./root/face_recognition/static/train/' + i))[0] for i in know_images]
 
     if len(img) == 0:
-        return jsonify([{"response": "An error hapenned, please choose another picture"}])
+        return jsonify(
+            [{"response": "An error hapenned, please choose another picture"}]
+        )
     elif len(img) > 1:
-        know_images_names = [i.rsplit('.', 1)[0] for i in know_images]
+        know_images_names = [i.rsplit(".", 1)[0] for i in know_images]
         results = []
         for i in img:
             matches = fr.compare_faces(know_images_encoded, i)
@@ -53,10 +57,13 @@ def detect_person(file_name):
     if results.count(False) == len(results):
         return jsonify([{"response": "Unknow person"}])
 
-    result = { i[0]:i[1] for i in zip(results, os.listdir('./root/face_recognition/static/train')) }
+    result = {
+        i[0]: i[1]
+        for i in zip(results, os.listdir("./root/face_recognition/static/train"))
+    }
 
-    #retorna apenas a classe(pessoa) com flag True
-    return jsonify([{"response": result[True].rsplit('.', 1)[0]}])
+    # retorna apenas a classe(pessoa) com flag True
+    return jsonify([{"response": result[True].rsplit(".", 1)[0]}])
 
 
 def send_file(path):
@@ -64,102 +71,114 @@ def send_file(path):
 
     jsonData = {}
 
-    #open file
-    files = {'file': open(path, 'rb')}
-    values = {'upload_preset': 'train_set'}
+    # open file
+    files = {"file": open(path, "rb")}
+    values = {"upload_preset": "train_set"}
 
-    #send file
+    # send file
     response = requests.post(url, files=files, data=values)
     print("\n\nStatus: " + str(response.status_code) + "\n\n")
-    if(response.status_code == 200):
+    if response.status_code == 200:
         jsonData = json.loads(response.text)
         print(jsonData)
 
     return jsonData
 
-#resources
-@app.route('/images', methods=['GET'])
+
+# resources
+@app.route("/images", methods=["GET"])
 def get_filenames():
-    if request.method == 'GET':
-        filename = [name.rsplit('.', 1)[0] for name in os.listdir("./root/face_recognition/static/train/")]
+    if request.method == "GET":
+        filename = [
+            name.rsplit(".", 1)[0]
+            for name in os.listdir("./root/face_recognition/static/train/")
+        ]
 
         return jsonify([{"filenames": filename}])
 
 
-@app.route('/upload_image', methods=['GET', 'POST'])
+@app.route("/upload_image", methods=["GET", "POST"])
 def upload_image():
-    if request.method == 'POST':
-        if 'file' not in request.files:
+    if request.method == "POST":
+        if "file" not in request.files:
             return redirect(request.url)
 
-        file = request.files['file']
+        file = request.files["file"]
 
-        if file.filename == '':
+        if file.filename == "":
             return redirect(request.url)
 
         if file and allowed_file(file.filename):
             return detect_person(file)
 
-    return  render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/add_image', methods=['GET', 'POST'])
+@app.route("/add_image", methods=["GET", "POST"])
 def add_image():
-    if request.method == 'POST':
-        if 'file' not in request.files:
+    if request.method == "POST":
+        if "file" not in request.files:
             return redirect(request.url)
 
-        file = request.files['file']
-        if file.filename == '':
+        file = request.files["file"]
+        if file.filename == "":
             return redirect(request.url)
 
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
 
-            #save file
-            saved_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            # save file
+            saved_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
             file.save(saved_path)
 
-            #load face
-            img = fr.load_image_file('./root/face_recognition/static/train/%s'%filename)
+            # load face
+            img = fr.load_image_file(
+                "./root/face_recognition/static/train/%s" % filename
+            )
             img = fr.face_encodings(img)
 
-            #save only face, if has more than one, don't will save it
+            # save only face, if has more than one, don't will save it
             if len(img) == 0 or len(img) >= 2:
-                os.system('rm ./root/face_recognition/static/train/%s' % filename)
+                os.system("rm ./root/face_recognition/static/train/%s" % filename)
                 return "An error happened, please choose another picture"
 
-            #send image to Cloudinary
+            # send image to Cloudinary
             send_file(saved_path)
 
-            #delete image
-            os.system('rm ./root/face_recognition/static/train/%s' % filename)
+            # delete image
+            os.system("rm ./root/face_recognition/static/train/%s" % filename)
 
-            #save encode of image
-            np.save('./root/face_recognition/static/train/%s'%filename.rsplit('.', 1)[0], img[0])
+            # save encode of image
+            np.save(
+                "./root/face_recognition/static/train/%s" % filename.rsplit(".", 1)[0],
+                img[0],
+            )
             return "Upload completed"
 
-    return  render_template('add-image-template.html')
+    return render_template("add-image-template.html")
 
 
-@app.route('/delete_image/<string:filename>', methods=['DELETE'])
+@app.route("/delete_image/<string:filename>", methods=["DELETE"])
 def delete_image(filename):
-    if request.method == 'DELETE':
+    if request.method == "DELETE":
 
         # filename = request.args.get('filename')
         filename = secure_filename(filename)
 
-        if(filename != ""):
-            files = [name.rsplit('.', 1)[0] for name in os.listdir("./root/face_recognition/static/train/")]
+        if filename != "":
+            files = [
+                name.rsplit(".", 1)[0]
+                for name in os.listdir("./root/face_recognition/static/train/")
+            ]
 
             exists = False
 
             for name in files:
-                if(name == filename):
+                if name == filename:
                     exists = True
 
             if exists:
-                os.system('rm ./root/face_recognition/static/train/%s.*' % filename)
+                os.system("rm ./root/face_recognition/static/train/%s.*" % filename)
                 return jsonify([{"response": "Removed"}])
 
             else:
@@ -167,32 +186,6 @@ def delete_image(filename):
 
         else:
             return "An error happened, please choose a filename"
-
-    #Old version
-
-    # if(request.is_json):
-    #
-    #     content = request.get_json()
-    #     filename = content['filename'].rsplit('.', 1)[0]
-    #
-    #     if(filename != ""):
-    #         files = [name.rsplit('.', 1)[0] for name in os.listdir("./root/face_recognition/static/train/")]
-    #
-    #         exists = False
-    #
-    #         for name in files:
-    #             if(name == filename):
-    #                 exists = True
-    #
-    #         if exists:
-    #             os.system('rm ./root/face_recognition/static/train/%s.*' % filename)
-    #             return jsonify([{"response": "Removed"}])
-    #
-    #         else:
-    #             return jsonify([{"response": "Not removed"}])
-    #
-    #     else:
-    #         return "An error happened, please choose a filename"
 
 
 if __name__ == "__main__":
